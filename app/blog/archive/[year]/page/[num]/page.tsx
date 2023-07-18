e6ccd67952b115a2f2efe8e0ae87ceef4fcaf82c
sato <whitedog0811@hotmail.co.jp>
2023-07-19 04:13:32 +0900

    [update] Pagenation

--- app/blog/archive/[year]/page/[num]/page.tsx
+++ app/blog/archive/[year]/page/[num]/page.tsx
@@ -1,9 +1,13 @@
-import { getList } from "../../../libs/microcms";
-import { ArticleList } from "../../../components/blog/ArticleList";
+import { getList, getDetail } from "../../../../../libs/microcms";
+import { ArticleList } from "../../../../../components/blog/ArticleList";
 import { Metadata, ResolvingMetadata } from 'next';
 
+
 type generateMetadataProps = {
-  params: { year: string }
+  params: {
+    year: string;
+    num: string;
+  }
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
@@ -11,14 +15,14 @@ export async function generateMetadata(
   { params, searchParams }: generateMetadataProps,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const year = params.year;
+  const { year, num } = params;
   
   return {
     metadataBase: new URL('https://portfolio-doe4gyax2-mormo.vercel.app'),
-    title: `${year}年 | 年別アーカイブ | ブログ | J.Sato Portfolio`,
-    description: `「${year}年」の一覧ページです。`,
+    title: `${num}ページ目 | ${year}年 | 年別アーカイブ | ブログ | J.Sato Portfolio`,
+    description: `「${year}年」の${num}ページ目です。`,
     openGraph: {
-      description:`「${year}年」の一覧ページです。`
+      description: `「${year}年」の${num}ページ目です。`
     }
   }
 }
@@ -26,16 +30,18 @@ export async function generateMetadata(
 type Props = {
   params: {
     year: string;
-  };
-};
+    num: string;
+  }
+}
 
 export default async function Page({ params }: Props) {
-  const { year } = params;
+  const { year, num } = params;
   const limit = 12;
   
   const { contents, totalCount } = await getList('blog', {
     filters: `publishedAt[contains]${year}`,
-    limit
+    limit,
+    offset: limit * (Number(num) - 1)
   });
 
   const type = {
@@ -44,5 +50,9 @@ export default async function Page({ params }: Props) {
     name: `${year}年`
   }
 
-  return (<ArticleList contents={contents} type={type} totalCount={totalCount} limit={limit} />)
+  return (
+    <>
+      <ArticleList contents={contents} type={type} totalCount={totalCount} limit={limit} currentPage={Number(num)} />
+    </>
+  )
 }
