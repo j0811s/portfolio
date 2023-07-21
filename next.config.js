f248ba6b0e3e7e4f78f3aab2dd9954f9ef14da1d
sato <whitedog0811@hotmail.co.jp>
2023-07-22 03:25:56 +0900

    [add] trailingSlash: true

--- next.config.js
+++ next.config.js
@@ -3,9 +3,13 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: 'export',
+  // distDir: 'dist',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io', 'placehold.jp']
+    // unoptimized: true
   }
 };
 
