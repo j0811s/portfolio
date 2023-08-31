d8227fb4f59bd1e689c1a92ee5efdeb6f3374383
sato <whitedog0811@hotmail.co.jp>
2023-08-31 21:16:13 +0900

    [fix] ページネーションの表示条件をmicroCMSブログ風に変更

--- app/components/blog/Pagenation/index.tsx
+++ app/components/blog/Pagenation/index.tsx
@@ -24,22 +24,57 @@ export const Pagenation = ({ pager, type }: PagenationParam) => {
     ? '/blog/page/'
     : `/blog/${type?.slug}/${type?.id}/page/`;
   
-  const currentCheck = (page: number): boolean => currentPage === (page + 1)
+  const currentPageCheck = (page: number): boolean => currentPage === (page + 1)
 
-  const pageNumbers = pageCount.map(page => (
-    <li key={page} className={`${listItem} ${currentCheck(page) ? current : ''}`}>
-      {
-        currentCheck(page)
-          ? <div className={`${currentText} ${pageLink}`}>{page + 1}</div>
-          : <Link className={pageLink} href={`${urlPath}${page + 1}`}>{page + 1}</Link>
-      }
-    </li>
-  ));
+  const firstPageNumber = () => {
+    if (3 >= currentPage) return <></>
+    return (
+      <>
+        <li className={`${listItem}`}>
+          <Link className={pageLink} href={`${urlPath}${1}`}>1</Link>
+        </li>
+        <li className={`${listItem}`} style={{ pointerEvents: "none"}}>
+          <div className={`${pageLink}`}>...</div>
+        </li>
+      </>
+    )
+  }
+  
+  const rangePageNumbers = pageCount.map(page => {
+    if (currentPage - 3 <= page && page <= currentPage + 1) {
+      return (
+        <li key={page} className={`${listItem} ${currentPageCheck(page) ? current : ''}`}>
+          {
+            currentPageCheck(page)
+              ? <div className={`${currentText} ${pageLink}`}>{page + 1}</div>
+              : <Link className={pageLink} href={`${urlPath}${page + 1}`}>{page + 1}</Link>
+          }
+        </li>
+      )
+    }
+  });
+
+  const lastPageNumber = () => {
+    if (currentPage + 2 < pageCount.length) {
+      return (
+        <>
+          <li className={`${listItem}`} style={{ pointerEvents: "none"}}>
+            <div className={`${pageLink}`}>...</div>
+          </li>
+          <li className={`${listItem}`}>
+            <Link className={pageLink} href={`${urlPath}${pageCount.length}`}>{pageCount.length}</Link>
+          </li>
+        </>
+      )
+    }
+  }
   
   return (
     <div className={container}>
       <ul className={list}>
-        {pageNumbers}
+        {firstPageNumber()}
+        {rangePageNumbers}
+        {lastPageNumber()}
       </ul>
     </div>
   )
