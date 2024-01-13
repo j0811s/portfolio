787c54c7d7854ae572b715d4aaae883b155eafc6
sato <whitedog0811@hotmail.co.jp>
2024-01-14 04:04:48 +0900

    [add] PickupArticles

---
+++ src/app/components/common/PickupArticles/index.tsx
@@ -0,0 +1,66 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDay, faClock } from "@fortawesome/free-solid-svg-icons";
+import {
+  postListEyecatchContainer,
+  postListEyecatch,
+  postList, postListItem, postListItemLink,
+  postData, postDataTitle, postDataDesc, postDateContainer,
+  postDateIcon, postDate,
+  postBtn
+} from './index.css';
+import Link from "next/link";
+import Image from 'next/image';
+import { Blog } from '@/src/app/libs/microcms/blog';
+import ConvertDate from "@/src/app/components/common/convertdate";
+
+type PickupArticlesParams = {
+  contents: Blog[];
+  totalCount: number;
+}
+
+export const PickupArticles = async ({ contents, totalCount }: PickupArticlesParams) => {
+  const hasContents = totalCount > 0;
+  
+  if (!hasContents) return (
+    <div className={postList}>
+      <p>投稿を準備中です。</p>
+    </div>
+  )
+
+  return (
+    <ul className={postList}>
+      {contents.map((post) => {
+        const eyecatchPath = `${post.eyecatch?.url}?fit=fill&w=634&h=356`;
+        return (
+          <li key={post.id} className={postListItem}>
+            <Link className={postListItemLink} href={`/blog/${post.id}`} scroll={true}>
+              <figure className={postListEyecatchContainer}>
+                {post.eyecatch ?
+                  <Image className={postListEyecatch} src={eyecatchPath} alt="" width={post.eyecatch.width} height={post.eyecatch.height} /> :
+                  <Image className={postListEyecatch} src="/images/blog/dummy.png" alt="" width="375" height="210" />
+                }
+              </figure>
+              <dl className={postData}>
+                <dt className={postDataTitle}>{post.title}</dt>
+                <dd className={postDataDesc}>
+                  <div className={postDateContainer}>
+                    <p className={postDate}>
+                      <FontAwesomeIcon className={postDateIcon} icon={faCalendarDay} />公開日：<ConvertDate date={post.publishedAt} />
+                    </p>
+                    {
+                      post.updatedAt &&
+                      <p className={postDate}>
+                        <FontAwesomeIcon className={postDateIcon} icon={faClock} />更新日：<ConvertDate date={post.updatedAt} />
+                      </p>
+                    }
+                  </div>
+                </dd>
+              </dl>
+              <div className={postBtn}>投稿を読む</div>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarDay, faClock } from "@fortawesome/free-solid-svg-icons";
 
 import { container } from "./styles/top/index.css";
-import { Blog } from '@/src/app/libs/microcms/blog';
 import { getList } from "@/src/app/libs/microcms/blog";
 import { getHistoryAllContents, type SkillContent } from "@/src/app/libs/microcms/history"
-import ConvertDate from "@/src/app/components/common/convertdate";
 import { SkillSet } from "./components/about/SkillSet";
-
-import {
-  postListWrapper,
-  postListEyecatchContainer, postListEyecatch,
-  postListTitle,
-  postList, postListItem, postListItemLink,
-  postData, postDataTitle, postDataDesc, postDateContainer,
-  postDateIcon, postDate,
-  postBtn
-} from '@/src/app/components/blog/ArticleList/index.css';
+import { PickupArticles } from "./components/common/PickupArticles";
 
 export default async function Top() {
   const skillContents: SkillContent[] = await getHistoryAllContents('skill');
@@ -27,46 +13,6 @@ export default async function Top() {
   const { contents, totalCount } = await getList('blog', {
     limit: PortfolioContentsLimit
   });
-
-  const PortfolioArticleList = () => {
-    
-    return (
-      <ul className={postList}>
-        {contents.map((post) => {
-          const eyecatchPath = `${post.eyecatch?.url}?fit=fill&w=634&h=356`;
-          return (
-            <li key={post.id} className={postListItem}>
-              <Link className={postListItemLink} href={`/blog/${post.id}`} scroll={true}>
-                <figure className={postListEyecatchContainer}>
-                  {post.eyecatch ?
-                    <Image className={postListEyecatch} src={eyecatchPath} alt="" width={post.eyecatch.width} height={post.eyecatch.height} /> :
-                    <Image className={postListEyecatch} src="/images/blog/dummy.png" alt="" width="375" height="210" />
-                  }
-                </figure>
-                <dl className={postData}>
-                  <dt className={postDataTitle}>{post.title}</dt>
-                  <dd className={postDataDesc}>
-                    <div className={postDateContainer}>
-                      <p className={postDate}>
-                        <FontAwesomeIcon className={postDateIcon} icon={faCalendarDay} />公開日：<ConvertDate date={post.publishedAt} />
-                      </p>
-                      {
-                        post.updatedAt &&
-                        <p className={postDate}>
-                          <FontAwesomeIcon className={postDateIcon} icon={faClock} />更新日：<ConvertDate date={post.updatedAt} />
-                        </p>
-                      }
-                    </div>
-                  </dd>
-                </dl>
-                <div className={postBtn}>投稿を読む</div>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    )
-  }
 
   return (
     <>
@@ -78,7 +24,7 @@ export default async function Top() {
       </section>
       <section>
         <h2>制作物</h2>
-        <PortfolioArticleList />
+        <PickupArticles contents={contents} totalCount={totalCount} />
         <Link href={`/blog/categories/portfolio/`}>もっと見る</Link>
       </section>
     </>
