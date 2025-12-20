c46b797c206c0694849327011ad68459f98d0a69
sato <whitedog0811@hotmail.co.jp>
2025-12-20 14:37:57 +0900

    [refactor]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { createVanillaExtractPlugin }from '@vanilla-extract/next-plugin';
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
   // output: 'export',
@@ -10,16 +8,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
       },
       {
-        protocol: 'https',
-        hostname: 'placehold.jp',
+        protocol: "https",
+        hostname: "placehold.jp",
       },
     ],
     // unoptimized: true
-  }
+  },
 };
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
