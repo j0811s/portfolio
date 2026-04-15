0a3bfb16cd7bdd0c2624e351e853fc6eb40ad880
sato <whitedog0811@hotmail.co.jp>
2026-04-15 16:35:07 +0900

    [add] Biome

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,9 +1,8 @@
-
 export const SITE_URL: string = process.env.SITE_URL || 'http://127.0.0.1:3000';
 
 export const SITE_META = {
   title: 'ポートフォリオサイト',
   sitename: 'ポートフォリオサイト',
-  description: "ポートフォリオサイト",
-  siteUrl: new URL(SITE_URL)
-}
+  description: 'ポートフォリオサイト',
+  siteUrl: new URL(SITE_URL),
+};
