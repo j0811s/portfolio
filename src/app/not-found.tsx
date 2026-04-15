1a4951b9189221ea86e252576a50b5d87347939d
sato <whitedog0811@hotmail.co.jp>
2026-04-15 22:55:29 +0900

    [update] 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,32 @@
 import styles from '@/src/styles/pages/not-found.module.css';
 import { CtaLinkButton, SectionTitle } from '@/src/components';
+import { AuthProvider } from '@/src/components/provider/AuthProvider';
+import { GlobalHeader } from '@/src/features/header';
+import { GlobalFooter } from '@/src/features/footer';
 
 export default function NotFound() {
   return (
-    <section className={styles.container}>
-      <SectionTitle title="404 | Page Not Found" level={1} />
-      <p className={styles.pageSubTitle}>ご指定のページが見つかりませんでした。</p>
-      <div className={styles.btn}>
-        <CtaLinkButton href={'/'} prevIcon={true}>
-          トップページに戻る
-        </CtaLinkButton>
-        <CtaLinkButton href={'/blog/'} nextIcon={true}>
-          投稿へ進む
-        </CtaLinkButton>
-      </div>
-    </section>
+    <AuthProvider>
+      <GlobalHeader />
+      <main>
+        <section className={styles.container}>
+          <p className={styles.errorCode}>404</p>
+          <div className={styles.titleWrapper}>
+            <SectionTitle title="Page Not Found" level={1} />
+            <p className={styles.pageSubTitle}>ご指定のページが見つかりませんでした。</p>
+            <div className={styles.divider} />
+          </div>
+          <div className={styles.btn}>
+            <CtaLinkButton href={'/'} prevIcon={true}>
+              トップページに戻る
+            </CtaLinkButton>
+            <CtaLinkButton href={'/blog/'} nextIcon={true}>
+              投稿を見る
+            </CtaLinkButton>
+          </div>
+        </section>
+      </main>
+      <GlobalFooter />
+    </AuthProvider>
   );
 }
